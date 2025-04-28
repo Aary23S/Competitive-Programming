62c0189bab1885d4be2204b72851888e3cd8e4a6
SAary <142585155+Aary23S@users.noreply.github.com>
2025-04-28 21:26:46 +0530

    Update Educational Codeforces Round 178 (Rated for Div. 2).cpp

--- CodeForces Contest/Div 2 Contest/Educational Codeforces Round 178 (Rated for Div. 2).cpp
+++ CodeForces Contest/Div 2 Contest/Educational Codeforces Round 178 (Rated for Div. 2).cpp
@@ -1,1 +1,33 @@
+//contest conducted on 28/4/25
 
+//https://codeforces.com/contest/2104/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        ll a, b, c;
+        cin >> a >> b >> c;
+
+        ll sum = a + b + c;
+
+        if (sum % 3 != 0) {
+            cout << "NO" << endl;
+            continue;
+        }
+
+        ll x = sum / 3;
+
+        if (x >= a && x >= b && x <= c && (c - x) >= 1) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
